50720407be48068eb34d9b50417a91e1bfc644e9
Martin Creedy <martin@openskydevelopment.co.uk>
2023-06-09 14:21:30 +0100

    Try catch

--- src/index.ts
+++ src/index.ts
@@ -36,41 +36,51 @@ app.post(`/profile`, upload.single('image'), async (req, res) => {
     return;
   }
 
-  let avatarCid;
-  if (file) {
+  try {
+    let avatarCid;
+    if (file) {
+      // We prefer this approach to the client.store() method as we just want the CID not the name of the file appended to the CID
+      const imageBlob = new Blob([file.buffer]);
+      avatarCid = await nftStorage.storeBlob(imageBlob);
+    }
+
+    const image = avatarCid ?? `ipfs://${avatarCid}`;
+
+    if (!avatarCid) {
+      console.log('No image provided');
+    } else {
+      console.log(`Image uploaded to IPFS with CID ${avatarCid}`);
+    }
+
+    const metadataBlob = new Blob(
+      [
+        JSON.stringify({
+          name,
+          description: `Embrace Community profile NFT for ${handle}`,
+          image,
+          properties: {
+            handle,
+          },
+        }),
+      ],
+      { type: 'application/json' },
+    );
+
     // We prefer this approach to the client.store() method as we just want the CID not the name of the file appended to the CID
-    const imageBlob = new Blob([file.buffer]);
-    avatarCid = await nftStorage.storeBlob(imageBlob);
+    const metadataCid = await nftStorage.storeBlob(metadataBlob);
+
+    console.log(`Metadata uploaded to IPFS with CID ${metadataCid}`);
+
+    console.log(
+      'Returning metadata CID and avatar CID',
+      metadataCid,
+      avatarCid,
+    );
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: 'Something went wrong' });
+    return;
   }
-
-  const image = avatarCid ?? `ipfs://${avatarCid}`;
-
-  if (!avatarCid) {
-    console.log('No image provided');
-  } else {
-    console.log(`Image uploaded to IPFS with CID ${avatarCid}`);
-  }
-
-  const metadataBlob = new Blob(
-    [
-      JSON.stringify({
-        name,
-        description: `Embrace Community profile NFT for ${handle}`,
-        image,
-        properties: {
-          handle,
-        },
-      }),
-    ],
-    { type: 'application/json' },
-  );
-
-  // We prefer this approach to the client.store() method as we just want the CID not the name of the file appended to the CID
-  const metadataCid = await nftStorage.storeBlob(metadataBlob);
-
-  console.log(`Metadata uploaded to IPFS with CID ${metadataCid}`);
-
-  console.log('Returning metadata CID and avatar CID', metadataCid, avatarCid);
 
   res.status(200).json({ metadataCid, avatarCid });
 });
